9cb79c1c8bcece6b9a8fd78c44027067baf57e75
LocNguyenDinh <>
2021-10-10 23:25:45 +0700

    'first'

---
+++ src/main/java/com/spring/HomeController.java
@@ -0,0 +1,17 @@
+package com.spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String get(){
+        return "Hello";
+    }
+    @GetMapping("/")
+    public  String get1(){
+        return "Welcome";
+    }
+}
